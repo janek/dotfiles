075816676b8368536c949f0a674a597ed74abc82
Janek Szynal <jan.szynal@gmail.com>
2020-12-15 23:16:21 +0100

    Add comments to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
+# Used for Android development
 set -x ANDROID_HOME /Users/janek/Library/Android/sdk
 set -x ANDROID_NDK_PATH $ANDROID_HOME/android-ndk-r16b
+
+# Used for Keyboard Maestro Spotify macros
 set -Ux SPOTIFY_AUTH_TOKEN_PATH /Users/janek/Developer/pi-spotify/spotify-alarm-clock/access_token.txt
 
 function fish_user_key_bindings
